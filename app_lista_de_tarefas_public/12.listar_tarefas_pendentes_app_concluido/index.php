e387462b328f463b3b5b498f523c854808cb4fbe
mrChernicharo <felipe.chernicharo@gmail.com>
2020-05-20 13:49:11 -0300

    app concluido

--- app_lista_de_tarefas_public/12.listar_tarefas_pendentes_app_concluido/index.php
+++ app_lista_de_tarefas_public/12.listar_tarefas_pendentes_app_concluido/index.php
@@ -1,16 +1,9 @@
 <?php
-
 	$acao = 'recuperar';
 	require "tarefa_controller.php";
+
 	
-	
-	/*
-			echo '<pre>';
-			print_r($tarefas);
-			echo '</pre>';
-	*/
-		
-	
+
 ?>
 
 
@@ -24,8 +17,7 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 		<script>
-		
-			function editar(id, txt_tarefa){
+		function editar(id, txt_tarefa){
 				//criar form de edição
 				let form = document.createElement('form')
 				form.action = 'tarefa_controller.php?acao=atualizar'
@@ -72,11 +64,11 @@
 			}
 
 			function remover(id){
-				location.href = 'todas_tarefas.php?acao=remover&id=' + id
+				location.href = 'index.php?acao=remover&id=' + id
 			}
 
 			function marcarRealizada(id){
-				location.href = 'todas_tarefas.php?acao=marcarRealizada&id=' + id
+				location.href = 'index.php?acao=marcarRealizada&id=' + id
 			}
 
 
@@ -96,40 +88,40 @@
 
 		<div class="container app">
 			<div class="row">
-				<div class="col-sm-3 menu">
+				<div class="col-md-3 menu">
 					<ul class="list-group">
-						<li class="list-group-item"><a href="index.php">Tarefas pendentes</a></li>
+						<li class="list-group-item active"><a href="#">Tarefas pendentes</a></li>
 						<li class="list-group-item"><a href="nova_tarefa.php">Nova tarefa</a></li>
-						<li class="list-group-item active"><a href="#">Todas tarefas</a></li>
+						<li class="list-group-item"><a href="todas_tarefas.php">Todas tarefas</a></li>
 					</ul>
 				</div>
 
-				<div class="col-sm-9">
+				<div class="col-md-9">
 					<div class="container pagina">
 						<div class="row">
 							<div class="col">
-								<h4>Todas tarefas</h4>
+								<h4>Tarefas pendentes</h4>
 								<hr />
-							
-							<? foreach($tarefas as $indice => $valor) { ?>
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9" id="tarefa_<?= $valor->id ?>" >
-										<?= ucfirst($valor->tarefa)  ?> (<?= ucfirst($valor->status)  ?>)
-									</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger" onclick="remover(<?= $valor->id ?>)"></i>
-										
-										<? if($valor->status == 'pendente') { ?>
-										<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $valor->id ?>, '<?= $valor->tarefa ?>')"></i>
-										<i class="fas fa-check-square fa-lg text-success" onclick="marcarRealizada(<?= $valor->id ?>)" ></i> 
-										<? } ?>	
+								<? foreach($tarefas as $indice => $valor) {
+									if ($valor->status == 'pendente') { ?>
+							<div class="row mb-3 d-flex align-items-center tarefa">
+								<div class="col-sm-9" id="tarefa_<?= $valor->id ?>" >
+									<?= ucfirst($valor->tarefa)  ?> (<?= ucfirst($valor->status)  ?>)
+								</div>
+								<div class="col-sm-3 mt-2 d-flex justify-content-between">
+									<i class="fas fa-trash-alt fa-lg text-danger" onclick="remover(<?= $valor->id ?>)"></i>
+									<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $valor->id ?>, '<?= $valor->tarefa ?>')"></i>
+									<i class="fas fa-check-square fa-lg text-success" onclick="marcarRealizada(<?= $valor->id ?>)" ></i> 
+								</div>
+							</div>
+		
+								<? } } ?>	
+
+
+						
 									</div>
 								</div>
-
-							<? } ?>
-							
-								
 							</div>
 						</div>
 					</div>
