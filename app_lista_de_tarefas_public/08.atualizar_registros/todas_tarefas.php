280ca366b799bdc7d730b0ff7115c2a1363e602a
mrChernicharo <felipe.chernicharo@gmail.com>
2020-05-20 13:44:18 -0300

    update de registros pt1

--- app_lista_de_tarefas_public/08.atualizar_registros/todas_tarefas.php
+++ app_lista_de_tarefas_public/08.atualizar_registros/todas_tarefas.php
@@ -23,6 +23,60 @@
 		<link rel="stylesheet" href="css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+		<script>
+		
+			function editar(id, txt_tarefa){
+				//criar form de edição
+				let form = document.createElement('form')
+				form.action = '#'
+				form.method = 'post'
+				form.className = 'row'
+
+				//criar input pra entrada do texto
+				let inputTarefa = document.createElement('input')
+				inputTarefa.type = 'text'
+				inputTarefa.name = 'tarefa'
+				inputTarefa.className = 'col-9 form-control'
+				inputTarefa.placeholder = txt_tarefa
+
+				//criar input hidden pra guardar o id da tarefa
+				let inputId = document.createElement('input')
+				inputId.type = 'hidden'
+				inputId.name = 'id'
+				inputId.value = id
+
+				//criar botão
+				let button = document.createElement('button')
+				button.type = 'submit'
+				button.className = 'col-3 btn btn-info'
+				button.innerHTML = 'Atualizar'
+
+				//incluir o input no form
+				form.appendChild(inputTarefa)
+
+				//incluir button no form
+				form.appendChild(button)
+
+				//incluir inputId no form
+				form.appendChild(inputId)
+				//console.log(form)
+				//alert(id)
+
+				//selecionar a div tarefa
+				let tarefa = document.getElementById('tarefa_'+ id)
+
+				//limpar o texto da div pra inclusão do form
+				tarefa.innerHTML = ''
+
+				//incluir form na pagina
+				tarefa.insertBefore(form, tarefa[0])
+
+				//alert(txt_tarefa)
+
+			}
+
+		</script>
+	
 	</head>
 
 	<body>
@@ -55,10 +109,12 @@
 							<? foreach($tarefas as $indice => $valor) { ?>
 
 								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9"><?= ucfirst($valor->tarefa)  ?> (<?= ucfirst($valor->status)  ?>)</div>
+									<div class="col-sm-9" id="tarefa_<?= $valor->id ?>" >
+										<?= ucfirst($valor->tarefa)  ?> (<?= ucfirst($valor->status)  ?>)
+									</div>
 									<div class="col-sm-3 mt-2 d-flex justify-content-between">
 										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
+										<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $valor->id ?>, '<?= $valor->tarefa ?>')"></i>
 										<i class="fas fa-check-square fa-lg text-success"></i>
 									</div>
 								</div>
