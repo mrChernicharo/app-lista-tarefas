7dee102932918538423aff7a6bbc4cd417290baf
mrChernicharo <felipe.chernicharo@gmail.com>
2020-05-20 13:43:23 -0300

    listar todos registros pt2

--- app_lista_de_tarefas_public/07.listar_todos_registros_parte2/todas_tarefas.php
+++ app_lista_de_tarefas_public/07.listar_todos_registros_parte2/todas_tarefas.php
@@ -2,7 +2,15 @@
 
 	$acao = 'recuperar';
 	require "tarefa_controller.php";
-
+	
+	
+	/*
+			echo '<pre>';
+			print_r($tarefas);
+			echo '</pre>';
+	*/
+		
+	
 ?>
 
 
@@ -43,9 +51,11 @@
 							<div class="col">
 								<h4>Todas tarefas</h4>
 								<hr />
+							
+							<? foreach($tarefas as $indice => $valor) { ?>
 
 								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Lavar o carro (status)</div>
+									<div class="col-sm-9"><?= ucfirst($valor->tarefa)  ?> (<?= ucfirst($valor->status)  ?>)</div>
 									<div class="col-sm-3 mt-2 d-flex justify-content-between">
 										<i class="fas fa-trash-alt fa-lg text-danger"></i>
 										<i class="fas fa-edit fa-lg text-info"></i>
@@ -53,14 +63,8 @@
 									</div>
 								</div>
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Passear com o cachorro (status)</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
-									</div>
-								</div>
+							<? } ?>
+							
 								
 							</div>
 						</div>
